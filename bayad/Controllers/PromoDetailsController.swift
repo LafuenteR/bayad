1ca33cb4476ddb03a472a33d63bb0f8668204d97
Ruel Lafuente <ruellafuente03@gmail.com>
2021-08-18 17:15:46 +0800

    added promodetailscontroller

---
+++ bayad/Controllers/PromoDetailsController.swift
@@ -0,0 +1,29 @@
+//
+//  PromoDetailsController.swift
+//  bayad
+//
+//  Created by Ruel Lafuente on 8/18/21.
+//
+
+import UIKit
+
+class PromoDetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
