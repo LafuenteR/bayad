05f704dc8839332a2bdd2c18a5dc038730f2c638
Ruel Lafuente <ruellafuente03@gmail.com>
2021-08-18 15:12:55 +0800

    Initial Commit

---
+++ bayad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bayad
+//
+//  Created by Ruel Lafuente on 8/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
